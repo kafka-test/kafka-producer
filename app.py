5cd9c3dc54422d4176766ecec4ee4da9856f82f1
hippyod <hippyod@yahoo.com>
2022-09-15 18:53:41 -0500

    testing

--- app.py
+++ app.py
@@ -2,14 +2,9 @@
 
 import os
 from kafka import KafkaProducer, KafkaAdminClient
-import json
-from bson import json_util
+from json import dumps
 from kafka.admin.new_partitions import NewPartitions
-import time
-    
-def send_message(counter, topic):
-    message = {'value': counter}
-    producer.send(topic, json.dumps(message, default = json_util.default).encode('utf-8'))
+from time import sleep
 
 # 'dev-kafka-cluster-kafka-ext-bootstrap-openshift-operators.apps-crc.testing:443'
 # 'stg-kafka-cluster-kafka-ext-bootstrap-openshift-operators.apps-crc.testing:443'
@@ -29,13 +24,14 @@ rsp = client.create_partitions({
 print("Producing messages to Kafka topic ...")
 producer = KafkaProducer(bootstrap_servers = kafka_server,
                          ssl_cafile = '/mnt/kafka-config/ca.crt',
-                         security_protocol="SSL")
+                         security_protocol="SSL",
+                         value_serializer=lambda x: dumps(x).encode('utf-8'))
 
 i = 0
 while True:
     i += 1
     msg = hostname + ' ' + str(i)
     topic = kafka_topic_1 if (i % 2) == 1 else kafka_topic_2
-    send_message(msg, topic)
-    time.sleep(0.5)
+    producer.send(topic, value = msg)
+    sleep(0.5)
   
